2a21d64a99bc0677998a75645f594addef7f3353
ryzhay <elenaaltukhova1@gmail.com>
2023-08-17 00:12:57 +0200

    update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
+  return new Promise ((resolve, reject) => {
   const shouldResolve = Math.random() > 0.3;
+  setTimeout(() => {
   if (shouldResolve) {
-    // Fulfill
+    resolve({position, delay});
   } else {
-    // Reject
+    reject({position, delay});
   }
+}, delay);
+});
+}
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit (event) {
+event.preventDefault();
+
+const delay = Number(event.target.elements.delay.value);
+const step = Number(event.target.elements.step.value);
+const amount = Number(event.target.elements.amount.value);
+
+for (let i = 0; i < amount; i += 1) {
+  const currentDelay = delay + i * step;
+
+createPromise(i + 1, currentDelay )
+  .then(({ position, delay }) => {
+    Notiflix.Report.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Report.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+}
 }
